47a719e18c7db4c7ab496bb8aef6827d95786c99
Kiran <kiran.bhujbal@wwindia.com>
2019-07-04 19:52:47 +0530

    List todos & delete on click

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { Component }  from 'react';
+import Todos from './Todos';
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state ={
+    todos :[
+      {id:1, content : 'Hey hows you?'},
+      {id:2 , content : 'Hi am fine'}
+    ]
+  }
+
+  deleteTodo = (id)=>{
+    console.log(id);
+    const todos = this.state.todos.filter(todo=>{
+      return todo.id !== id;
+    })
+   // this.setState(todos)
+    this.setState({todos : todos})
+  }
+
+  render() {
+    return (
+      <div className="App-todo container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
 }
-
 export default App;
