3bf6240ce04b417f68fa63851ed30b044fb0498c
Kiran <kiran.bhujbal@wwindia.com>
2019-07-04 21:12:26 +0530

    form to add new todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React , { Component }  from 'react';
 import Todos from './Todos';
+
+import AddTodo from './AddTodo';
+
 class App extends Component{
 
   state ={
@@ -17,12 +20,18 @@ class App extends Component{
    // this.setState(todos)
     this.setState({todos : todos})
   }
+  addTodo =(todo)=>{
+    todo.id = Math.random();
+    let todos = [...this.state.todos , todo];
+    this.setState({todos})
+  }
 
   render() {
     return (
       <div className="App-todo container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
