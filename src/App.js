dfe213b98ec02ec513c9d78fcd1cafbe78d6fcff
Kiran <kiran.bhujbal@wwindia.com>
2019-07-07 00:10:21 +0530

    Routing using Switch to match exact url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React , { Component }  from 'react';
 
 import Navbar from './components/Navbar';
 
-import { BrowserRouter ,Route } from 'react-router-dom';
+import { BrowserRouter ,Route ,Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import About from './components/About';
@@ -20,13 +20,14 @@ class App extends Component{
         <BrowserRouter>
         <div className="App"> 
           <Navbar/>
+          <Switch>
           <Route exact path='/home' component={Home} />
           <Route path='/contact' component={Contact} />
           <Route path='/about' component={About} />
           <Route path='/posts' component={Posts} />
           <Route path='/todo' component={Todo} />
           <Route path='/:post_id' component={Post} />
-
+          </Switch>
         </div>
         </BrowserRouter>
        
