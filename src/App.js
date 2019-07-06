b072295ba6fb9afff20c2d0f1779e02be9dbba9e
Kiran <kiran.bhujbal@wwindia.com>
2019-07-06 22:32:14 +0530

    Grab url params inside class component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Contact from './components/Contact';
 import About from './components/About';
 import Posts from './components/Posts';
 import Todo from './components/Todo';
+import Post from './components/Post';
 
 class App extends Component{
 
@@ -19,11 +20,12 @@ class App extends Component{
         <BrowserRouter>
         <div className="App"> 
           <Navbar/>
-          <Route path='/home' component={Home} />
+          <Route exact path='/home' component={Home} />
           <Route path='/contact' component={Contact} />
           <Route path='/about' component={About} />
           <Route path='/posts' component={Posts} />
           <Route path='/todo' component={Todo} />
+          <Route path='/:post_id' component={Post} />
 
         </div>
         </BrowserRouter>
